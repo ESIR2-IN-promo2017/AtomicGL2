cf1fb9a04def96b651554b191465ceb39fc2e2ed
Karkariy <helali44@hotmail.fr>
2016-02-08 23:32:48 +0100

    Importer Obj ok

--- script/webgl/atomicGL2Importer.js
+++ script/webgl/atomicGL2Importer.js
@@ -11,23 +11,23 @@
 
 /*
 This Class will import different type of Mesh
-	1° : .Obj
-	2° : .fbx (I hope)
-	3° : we will see
+  1° : .Obj
+  2° : .fbx (I hope)
+  3° : we will see
 */
 
 "use strict";
 
 class atomicGL2Importer  {
-	constructor(agl,path){
+  constructor(agl,path){
 
-	this.file = this.readTextFile(path);
-	this.obj = this.importObj(this.file);
-	this.obj3d = this.build(this.obj);
+  this.file = this.readTextFile(path);
+  this.obj = this.importObj(this.file);
+  this.obj3d = this.build(this.obj);
 
-	this.obj3d.initGLBuffers(agl);
-	agl.shapes.push(this.obj3d);
-	}
+  this.obj3d.initGLBuffers(agl);
+  agl.shapes.push(this.obj3d);
+  }
 
  readTextFile(file){
     var rawFile = new XMLHttpRequest();
@@ -49,53 +49,52 @@ class atomicGL2Importer  {
 
 importObj(fileText){
 
-	//the object3D
-	var obj = [];
+  //the object3D
+  var obj = [];
 
-	obj.name ='';
-	// vertices array
-	obj.vertices = [];
-	// normals array
-	obj.normals  = [] ;
-	// indexes array
-	obj.vertexIndices = [];
-	//texture coordinates
-	obj.uv = [];
+  obj.name ='';
+  // vertices array
+  obj.vertices = [];
+  // normals array
+  obj.normals  = [] ;
+  // indexes array
+  obj.vertexIndices = [];
+  //texture coordinates
+  obj.uv = [];
+  //obj index
+  obj.index = 0;
 
-	obj.index = 3 ;
- var verts = [], vertNormals = [], textures = [], unpacked = {};
-    // unpacking stuff
-    unpacked.verts = [];
-    unpacked.norms = [];
-    unpacked.textures = [];
-    unpacked.hashindices = {};
-    unpacked.indices = [];
-    unpacked.index = 0;
+
+ var  objTmp = {};
+    objTmp.normals = [];
+    objTmp.textures = [];
+    objTmp.hashindices = {};
+    objTmp.indices = [];
     // array of lines separated by the newline
     var lines = fileText.split('\n');
 
-    var VERTEX_RE = /^v\s/;
-    var NORMAL_RE = /^vn\s/;
-    var TEXTURE_RE = /^vt\s/;
-    var FACE_RE = /^f\s/;
     var WHITESPACE_RE = /\s+/;
 
     for (var i = 0; i < lines.length; i++) {
       var line = lines[i].trim();
       var elements = line.split(WHITESPACE_RE);
-      elements.shift();
+      var firstChar = elements.shift();
 
-      if (VERTEX_RE.test(line)) {
+      if(firstChar == '#'){
+        continue;
+      } else if (firstChar == 'g' ||firstChar == 'o') {
+        obj.name = elements;
+      }else if (firstChar == 'v') {
         // if this is a vertex
 
-        verts.push.apply(verts, elements);
-      } else if (NORMAL_RE.test(line)) {
+        obj.vertices.push.apply(obj.vertices, elements);
+      } else if (firstChar == 'vn') {
         // if this is a vertex normal
-        vertNormals.push.apply(vertNormals, elements);
-      } else if (TEXTURE_RE.test(line)) {
+        objTmp.normals.push.apply(objTmp.normals, elements);
+      } else if (firstChar == 'vt') {
         // if this is a texture
-        textures.push.apply(textures, elements);
-      } else if (FACE_RE.test(line)) {
+        objTmp.textures.push.apply(objTmp.textures, elements);
+      } else if (firstChar == 'f') {
         // if this is a face
         /*
         split this face into an array of vertex groups
@@ -114,86 +113,61 @@ importObj(fileText){
             //      'f v2/t2/vn2 v3/t3/vn3 v0/t0/vn0'
             if(j === 3 && !quad) {
                 // add v2/t2/vn2 in again before continuing to 3
-                j = 2;
+               
                 quad = true;
             }
-            if(elements[j] in unpacked.hashindices){
-                unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                /*
+            Each element of the face line array is a vertex which has its
+            attributes delimited by a forward slash. This will separate
+            each attribute into another array:
+                '19/92/11'
+            becomes:
+                vertex = ['19', '92', '11'];
+            where
+                vertex[0] is the vertex index
+                vertex[1] is the texture index
+                vertex[2] is the normal index
+             Think of faces having Vertices which are comprised of the
+             attributes location (v), texture (vt), and normal (vn).
+             */
+            var vertex = elements[ j ].split( '/' );
+            // vertex position sorted with in vertexIndices order
+            /*objTmp.verts.push(+ obj.vertices[(vertex[0] - 1) * 3 + 1]);
+            objTmp.verts.push(+ obj.vertices[(vertex[0] - 1) * 3 + 2]);
+            objTmp.verts.push(+ obj.vertices[(vertex[0] - 1) * 3 + 0]);
+            */
+            // vertex textures
+            if (objTmp.textures.length) {
+              obj.uv.push(+objTmp.textures[(vertex[1] - 1) * 2 + 0]);
+              obj.uv.push(+objTmp.textures[(vertex[1] - 1) * 2 + 1]);
             }
-            else{
-                /*
-                Each element of the face line array is a vertex which has its
-                attributes delimited by a forward slash. This will separate
-                each attribute into another array:
-                    '19/92/11'
-                becomes:
-                    vertex = ['19', '92', '11'];
-                where
-                    vertex[0] is the vertex index
-                    vertex[1] is the texture index
-                    vertex[2] is the normal index
-                 Think of faces having Vertices which are comprised of the
-                 attributes location (v), texture (vt), and normal (vn).
-                 */
-                var vertex = elements[ j ].split( '/' );
-                /*
-                 The verts, textures, and vertNormals arrays each contain a
-                 flattend array of coordinates.
-                 Because it gets confusing by referring to vertex and then
-                 vertex (both are different in my descriptions) I will explain
-                 what's going on using the vertexNormals array:
-                 vertex[2] will contain the one-based index of the vertexNormals
-                 section (vn). One is subtracted from this index number to play
-                 nice with javascript's zero-based array indexing.
-                 Because vertexNormal is a flattened array of x, y, z values,
-                 simple pointer arithmetic is used to skip to the start of the
-                 vertexNormal, then the offset is added to get the correct
-                 component: +0 is x, +1 is y, +2 is z.
-                 This same process is repeated for verts and textures.
-                 */
-                // vertex position sorted with in vertexIndices order
-              
-              /*  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
-                unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
-                unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
-              */
+            // vertex normals
+            obj.normals.push(+objTmp.normals[(vertex[2] - 1) * 3 + 0]);
+            obj.normals.push(+objTmp.normals[(vertex[2] - 1) * 3 + 1]);
+            obj.normals.push(+objTmp.normals[(vertex[2] - 1) * 3 + 2]);
+            // add the newly created vertex to the list of indices
+            obj.vertexIndices.push(vertex[0]);
+            // increment the counter
+            obj.index += 1;
 
-                // vertex textures
-                if (textures.length) {
-                  unpacked.textures.push(+textures[(vertex[1] - 1) * 2 + 0]);
-                  unpacked.textures.push(+textures[(vertex[1] - 1) * 2 + 1]);
-                }
-                // vertex normals
-                unpacked.norms.push(+vertNormals[(vertex[2] - 1) * 3 + 0]);
-                unpacked.norms.push(+vertNormals[(vertex[2] - 1) * 3 + 1]);
-                unpacked.norms.push(+vertNormals[(vertex[2] - 1) * 3 + 2]);
-                // add the newly created vertex to the list of indices
-                unpacked.hashindices[elements[j]] = unpacked.index;
-                unpacked.indices.push(unpacked.index);
-                // increment the counter
-                unpacked.index += 1;
-            }
             if(j === 3 && quad) {
-                // add v0/t0/vn0 onto the second triangle
-                unpacked.indices.push( unpacked.hashindices[elements[0]]);
+                // replace the fourth vertex of the quad which is the last element of obj.vertexIndices
+                // in order to split the quad onto two triangle : 
+                // f 1 2 3 4 => 1 2 3  1 3 4 
+              obj.vertexIndices[obj.vertexIndices.length-1] =  obj.vertexIndices[obj.vertexIndices.length-4];
+               obj.vertexIndices.push(obj.vertexIndices[obj.vertexIndices.length-2],
+                                      vertex[0]);
             }
         }
       }
     }
-
-    //obj.vertices = unpacked.verts;
-    obj.vertices = verts;
-    obj.normals = unpacked.norms;
-    obj.uv = unpacked.textures;
-    obj.vertexIndices = unpacked.indices;
-	console.log( obj.uv );
-	return obj;
+  return obj;
 
 }
 
 
-	build(obj){
-		return new atomicGL2ObjMesh(obj.name,obj,1.0,1.0);
-	}
+  build(obj){
+    return new atomicGL2ObjMesh(obj.name,obj,1.0,1.0);
+  }
 
 }
