7c36970dfbbfeaa683959fee3c1589b0bba573c3
LucasFoucault <foucault.lucas@gmail.com>
2016-03-22 17:47:17 +0100

    Add new camera system

--- script/webGLStart.js
+++ script/webGLStart.js
@@ -1,31 +1,32 @@
-	// for mouse
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
-	var mouseX = 0.0;
-	var mouseY = 0.0;
-	document.onmousemove = onDocumentMouseMove;
+// for mouse
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
-	var currentlyPressedKeys = {};
-	//	keyboard callbacks
-	document.onkeydown = handleKeyDown;
-	document.onkeyup = handleKeyUp;
+var mouseY;
+var mouseX;
 
-	// atomicGL
-	// -------------------------------------------------
-	// GL context
-	var agl = new atomicGL2Context();
-	// matrix stack
-	var ams = new atomicGL2MatrixStack();
-	// clock
-	var sceneClock = new atomicGL2Clock();
-	// -------------------------------------------------
+document.onmousemove = handleMouseMove;
+
+var currentlyPressedKeys = {};
+//	keyboard callbacks
+document.onkeydown 	= handleKeyDown;
+document.onkeyup 	= handleKeyUp;
+
+// atomicGL
+// -------------------------------------------------
+// GL context
+var agl = new atomicGL2Context();
+// matrix stack
+var ams = new atomicGL2MatrixStack();
+// clock
+var sceneClock = new atomicGL2Clock();
+// -------------------------------------------------
 
 
 // draw
 // -----------------------------
-function sceneDraw(){
+function sceneDraw() {
 	agl.initDraw();
-
 	agl.scenegraph.draw(agl,ams);
 }
 
@@ -33,66 +34,105 @@ function sceneDraw(){
 // -----------------------------
 function nextFrame() {
 	handleKeys();
-  requestAnimFrame(nextFrame);
-  sceneDraw();
-  animate();
+  	requestAnimFrame(nextFrame);
+  	sceneDraw();
+  	animate();
 }
 
 // animate
 // ------------------------------
-function animate(){
+function animate() {
 	// increase time
-	sceneClock.tick() ;
+	sceneClock.tick();
 }
 
-// keyboard
-// --------------------------------
-function handleKeyDown(event) 	{currentlyPressedKeys[event.keyCode] = true;}
-function handleKeyUp(event) 	{currentlyPressedKeys[event.keyCode] = false;}
+// KEYBOARD
+function handleKeyDown(event) {
+	currentlyPressedKeys[event.keyCode] = true;
+}
+
+function handleKeyUp(event) {
+	currentlyPressedKeys[event.keyCode] = false;
+}
 
 function handleKeys() {
-	if (Math.abs(mouseX)>0.1){
-		agl.scenegraph.camera.turnright(1.0*(mouseX*mouseX*mouseX));
+
+	if (currentlyPressedKeys[32]) {
+		agl.scenegraph.camera.isFreeCam = !agl.scenegraph.camera.isFreeCam;
+		agl.scenegraph.camera.up();
 	}
-	agl.scenegraph.camera.turnup(45*mouseY);
-	if (currentlyPressedKeys[67]) // (C) debug
+
+	if(agl.scenegraph.camera.isFreeCam)
 	{
-		// debug
-		console.log('atomicGL - Rémi COZOT - 2015');
+		// (Z) Up
+		if (currentlyPressedKeys[90]) {
+			agl.scenegraph.camera.step += 0.05;
+		}
+		// (S) Down
+		if (currentlyPressedKeys[83]) {
+			agl.scenegraph.camera.step -= 0.05;
+		}
+
+		console.log('speed : ', agl.scenegraph.camera.step);
+		agl.scenegraph.camera.up();
 	}
-	if (currentlyPressedKeys[68]) // (D) Right
+	else
 	{
-		agl.scenegraph.camera.right();
-	}
-	if (currentlyPressedKeys[81]) // (Q) Left
-	{
-		agl.scenegraph.camera.left();			//
-	}
-	if (currentlyPressedKeys[90]) // (Z) Up
-	{
-		agl.scenegraph.camera.up();			//
-	}
-	if (currentlyPressedKeys[83]) // (S) Down
-	{
-		agl.scenegraph.camera.down();			//
+		agl.scenegraph.camera.step = 0.5;
+
+		// (C) debug
+		if (currentlyPressedKeys[67]) { 
+			console.log('atomicGL - Rémi COZOT - 2015');
+		}
+		// (D) Right
+		if (currentlyPressedKeys[68]) {
+			agl.scenegraph.camera.right();
+		}
+		// (Q) Left
+		if (currentlyPressedKeys[81]) {
+			agl.scenegraph.camera.left();
+		}
+		// (Z) Up
+		if (currentlyPressedKeys[90]) {
+			agl.scenegraph.camera.up();
+		}
+		// (S) Down
+		if (currentlyPressedKeys[83]) {
+			agl.scenegraph.camera.down();
+		}
 	}
 }
 
-// mouse
-// ------------------------------
-function onDocumentMouseMove( event ) {
-	omouseX = mouseX ;
-	mouseX  = ( event.clientX - windowHalfX ) / windowHalfX;
-	mouseY  = ( event.clientY - windowHalfY ) / windowHalfY;
+// MOUSE
+function handleMouseMove(event) {
+	mouseX = event.clientX;
+	mouseY = event.clientY;
 }
 
-	//webGLStart
-function webGLStart() {
+function degToRad(degrees) {
+	var result = Math.PI/180 * degrees;
+	return result;
+}
+
+function canvasDraw(agl, canvas) {
+	if(mouseX != undefined && mouseY != undefined)
+	{
+		agl.scenegraph.camera.turnright(mouseX);
+		agl.scenegraph.camera.turnup(mouseY);
+
+		mouseY = 0;
+		mouseX = 0;
+	}
+}
+
+//webGLStart
+function webGLStart() 
+{
 	// init
 	// -----------------------------
 	// recover OpenGL canvas
+	var element = document.pointerLockElement;
 	var canvas = document.getElementById("oglcanvas");
-
 	// init OpenGL context
 	// canvas, background color
 	agl.initGL(canvas,[0.15,0.1,0.5]);
@@ -101,8 +141,51 @@ function webGLStart() {
 	var scene = document.getElementById('id').innerHTML;
 	var sgxml = new atomicGL2xml(agl,'scenes/'+scene+'.xml');
 
+	// pointer lock object forking for cross browser
+	canvas.requestPointerLock = 
+		canvas.requestPointerLock ||
+		canvas.mozRequestPointerLock;
+
+	document.exitPointerLock = 
+		document.exitPointerLock ||
+		document.mozExitPointerLock;
+
+	canvas.onclick = function() {
+		canvas.requestPointerLock();
+	}
+
+	// Hook pointer lock state change events for different browsers
+	document.addEventListener('pointerlockchange', lockChangeAlert, false);
+	document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+
+	// Hook mouse move events
+	document.addEventListener("mousemove", this.moveCallback, false);
+
+	function lockChangeAlert() {
+		if(document.pointerLockElement === canvas ||
+		document.mozPointerLockElement === canvas) {
+			console.log('The pointer lock status is now locked');
+	    	document.addEventListener("mousemove", canvasLoop, false);
+		} else {
+	    	console.log('The pointer lock status is now unlocked');  
+	    	document.removeEventListener("mousemove", canvasLoop, false);
+	  	}
+	}
+
+	function canvasLoop(e) {
+		var movementX = e.mozMovementX;
+		var movementY = e.mozMovementY;
+
+		mouseX = movementX;
+		mouseY = movementY;
+
+		canvasDraw(agl, canvas);
+
+		var animation = requestAnimationFrame(canvasLoop);
+	}
+
 	// init Matrix Stack
-	ams.initMatrix(agl,45); // fov = 45 degrees
+	ams.initMatrix(agl, 45); // fov = 45 degrees
 
 	// start the animation
 	nextFrame();
--- script/webgl/atomicGLWalkCamera.js
+++ script/webgl/atomicGLWalkCamera.js
@@ -31,6 +31,16 @@ class atomicGLWalkCamera {
 	// rot
 	this.rot = 0.5 ;
 
+  this.isFreeCam = false;
+/*
+  // Vecteur de position et de cible
+  Vector3D position;
+  Vector3D target;
+
+  // 
+  Vector3D forward;
+  Vector3D left;
+*/
   // navmesh
   this.navmesh = new atomicGL2ObjMesh("navigation", new sol_herbe(), 1, 1);
 }
@@ -64,66 +74,98 @@ class atomicGLWalkCamera {
 	}
 
   move(displaceX, displaceZ) {
-    for (var i=0; i<this.navmesh.vertexIndices.length; i+=3)
+    if(this.isFreeCam)
     {
-      var ptx = this.xc + displaceX;
-      var ptz = this.zc + displaceZ;
+      this.xc += displaceX;
+      this.zc += displaceZ;
 
-      var v1x = this.navmesh.verticesArray[this.navmesh.vertexIndices[i]*3];
-      var v1y = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i]*3)+1];
-      var v1z = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i]*3)+2];
-      var v2x = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i+1])*3];
-      var v2y = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+1])*3)+1];
-      var v2z = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+1])*3)+2];
-      var v3x = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i+2])*3];
-      var v3y = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+2])*3)+1];
-      var v3z = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+2])*3)+2];
-
-      var b1 = (((ptx - v2x) * (v1z - v2z)) - ((v1x - v2x) * (ptz - v2z))) < 0.0;
-      var b2 = (((ptx - v3x) * (v2z - v3z)) - ((v2x - v3x) * (ptz - v3z))) < 0.0;
-      var b3 = (((ptx - v1x) * (v3z - v1z)) - ((v3x - v1x) * (ptz - v1z))) < 0.0;
-
-      if((b1 == b2) && (b2 == b3))
+      this.yc += -this.step*Math.sin(this.phi*3.14/180.0);
+    }
+    else
+    {
+      for (var i=0; i<this.navmesh.vertexIndices.length; i+=3)
       {
-        var a = Math.sqrt(Math.pow(v1x-v2x, 2) + Math.pow(v1z-v2z, 2));
-        var b = Math.sqrt(Math.pow(v2x-v3x, 2) + Math.pow(v2z-v3z, 2));
-        var c = Math.sqrt(Math.pow(v3x-v1x, 2) + Math.pow(v3z-v1z, 2));
+        var ptx = this.xc + displaceX;
+        var ptz = this.zc + displaceZ;
 
-        var da = Math.sqrt(Math.pow(ptx-v1x, 2) + Math.pow(ptz-v1z, 2));
-        var db = Math.sqrt(Math.pow(ptx-v2x, 2) + Math.pow(ptz-v2z, 2));
-        var dc = Math.sqrt(Math.pow(ptx-v3x, 2) + Math.pow(ptz-v3z, 2));
+        var v1x = this.navmesh.verticesArray[this.navmesh.vertexIndices[i]*3];
+        var v1y = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i]*3)+1];
+        var v1z = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i]*3)+2];
+        var v2x = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i+1])*3];
+        var v2y = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+1])*3)+1];
+        var v2z = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+1])*3)+2];
+        var v3x = this.navmesh.verticesArray[(this.navmesh.vertexIndices[i+2])*3];
+        var v3y = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+2])*3)+1];
+        var v3z = this.navmesh.verticesArray[((this.navmesh.vertexIndices[i+2])*3)+2];
 
-        var p = (a+b+c)/2;
-        var pA = (a+da+db)/2;
-        var pB = (b+db+dc)/2;
-        var pC = (c+dc+da)/2;
+        var b1 = (((ptx - v2x) * (v1z - v2z)) - ((v1x - v2x) * (ptz - v2z))) < 0.0;
+        var b2 = (((ptx - v3x) * (v2z - v3z)) - ((v2x - v3x) * (ptz - v3z))) < 0.0;
+        var b3 = (((ptx - v1x) * (v3z - v1z)) - ((v3x - v1x) * (ptz - v1z))) < 0.0;
 
-        var Air = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-        var AirA = Math.sqrt(pA*(pA-a)*(pA-da)*(pA-db));
-        var AirB = Math.sqrt(pB*(pB-b)*(pB-db)*(pB-dc));
-        var AirC = Math.sqrt(pC*(pC-c)*(pC-dc)*(pC-da));
+        if((b1 == b2) && (b2 == b3))
+        {
+          var a = Math.sqrt(Math.pow(v1x-v2x, 2) + Math.pow(v1z-v2z, 2));
+          var b = Math.sqrt(Math.pow(v2x-v3x, 2) + Math.pow(v2z-v3z, 2));
+          var c = Math.sqrt(Math.pow(v3x-v1x, 2) + Math.pow(v3z-v1z, 2));
 
-        var alpha = AirA / Air;
-        var beta = AirB / Air;
-        var gamma = AirC / Air;
+          var da = Math.sqrt(Math.pow(ptx-v1x, 2) + Math.pow(ptz-v1z, 2));
+          var db = Math.sqrt(Math.pow(ptx-v2x, 2) + Math.pow(ptz-v2z, 2));
+          var dc = Math.sqrt(Math.pow(ptx-v3x, 2) + Math.pow(ptz-v3z, 2));
 
-        this.xc +=	displaceX;
-        this.zc += 	displaceZ;
-        this.yc = (alpha*v3y + beta*v1y + gamma*v2y) + 2;
+          var p = (a+b+c)/2;
+          var pA = (a+da+db)/2;
+          var pB = (b+db+dc)/2;
+          var pC = (c+dc+da)/2;
 
-        break;
+          var Air = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+          var AirA = Math.sqrt(pA*(pA-a)*(pA-da)*(pA-db));
+          var AirB = Math.sqrt(pB*(pB-b)*(pB-db)*(pB-dc));
+          var AirC = Math.sqrt(pC*(pC-c)*(pC-dc)*(pC-da));
+
+          var alpha = AirA / Air;
+          var beta = AirB / Air;
+          var gamma = AirC / Air;
+
+          this.xc +=	displaceX;
+          this.zc += 	displaceZ;
+          this.yc = (alpha*v3y + beta*v1y + gamma*v2y) + 2;
+
+          break;
+        }
       }
     }
   }
 
-	turnright(a) {
-		this.theta += +a ;
-	}
+	turnright(a) 
+  {this.theta += a;}
 
-  turnleft(a) {
-		this.theta += +a ;
-	}
+  turnleft(a) 
+  {this.theta = a;}
 
-  turnup(a){this.phi = a
-  }
+  turnup(a)
+  {this.phi += a;}
+
+/*
+  vectorsFromAngles()
+  {
+    static const Vector3D up(0,0,1);
+
+    if (phi > 89)
+        phi = 89;
+
+    else if (phi < -89)
+        phi = -89;
+
+
+    double r_temp = cos(phi*M_PI/180);
+
+    forward.Z = sin(phi*M_PI/180);
+    forward.X = r_temp*cos(theta*M_PI/180);
+    forward.Y = r_temp*sin(theta*M_PI/180);
+
+    left = up.crossProduct(forward);
+    left.normalize();
+
+    target = position + forward;
+  }*/
 }
